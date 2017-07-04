83b6d57762d164918dd5e4dcc266e884ee846f52
PawSzymanski <pszyma777@gmail.com>
2017-07-05 01:41:58 +0200

    stage manager

---
+++ LibsAndDeclarations.h
@@ -0,0 +1,7 @@
+#pragma once
+#include <iostream>
+#include <cmath>
+#include <memory>
+#include <SFML\Graphics.hpp>
+#include <SFML\Audio.hpp>
+#include <entityx\entityx.h>
---
+++ ResourceManager.cpp
@@ -0,0 +1,29 @@
+#include "ResourceManager.h"
+
+ResourcesManager* ResourcesManager::instance = nullptr;
+
+ResourcesManager::ResourcesManager() : window(sf::VideoMode(1366, 768, 32), "Astrum", sf::Style::Titlebar | sf::Style::Close)
+{
+	window.setVerticalSyncEnabled(true);
+}
+
+ResourcesManager::~ResourcesManager()
+{
+}
+
+ResourcesManager* ResourcesManager::getInstance()
+{
+	if (!instance) instance = new ResourcesManager();
+
+	return instance;
+}
+
+ResourcesManager& ResourcesManager::getInstanceRef()
+{
+	return *getInstance();
+}
+
+void ResourcesManager::deleteInstance()
+{
+	delete instance;
+}
